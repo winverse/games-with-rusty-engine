e4a8a58673713862b5c0cfd54ba0c4698b6e4ca5
sangHyun <public.winverse@gmail.com>
2023-02-09 15:10:54 +0900

    Feat: Completed road race

---
+++ road-race/src/main.rs
@@ -0,0 +1,137 @@
+use rand::{thread_rng, Rng};
+use rusty_engine::prelude::*;
+
+const PLAYER_SPEED: f32 = 250.0;
+const ROAD_SPEED: f32 = 400.0;
+
+struct GameState {
+    health_amount: u8,
+    lost: bool,
+}
+impl Default for GameState {
+    fn default() -> Self {
+        Self {
+            health_amount: 5,
+            lost: false,
+        }
+    }
+}
+
+fn main() {
+    let mut game = Game::new();
+
+    // Create new Player
+    let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
+    player.translation.x = -500.0;
+    player.layer = 10.0;
+    player.collision = true;
+
+    // Set background music
+    game.audio_manager
+        .play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+
+    // create the road lines
+    for i in 0..10 {
+        let label = format!("roadline_{}", i);
+        let roadline = game.add_sprite(label, SpritePreset::RacingBarrierWhite);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    // Create the obstacle
+    let obstacle_presets = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+        SpritePreset::RacingConeStraight,
+        SpritePreset::RacingConeStraight,
+        SpritePreset::RollingBlockCorner,
+        SpritePreset::RollingBlockSquare,
+        SpritePreset::RollingBlockSmall,
+    ];
+
+    for (i, preset) in obstacle_presets.into_iter().enumerate() {
+        let label = format!("obstacle_{}", i);
+        let obstacle = game.add_sprite(label, preset);
+        obstacle.layer = 5.0;
+        obstacle.collision = true;
+        obstacle.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstacle.translation.y = thread_rng().gen_range(-300.0..300.0);
+    }
+
+    // Create Health message
+    let health_message = game.add_text("health_message", "Health: 5");
+    health_message.translation = Vec2::new(555.0, 320.0);
+
+    game.add_logic(game_logic);
+    game.run(GameState {
+        ..Default::default()
+    });
+}
+
+fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
+    if game_state.lost {
+        return;
+    }
+
+    // Collect Keyboard Input
+    let mut direction = 0.0;
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Up, KeyCode::W, KeyCode::Comma])
+    {
+        direction += 1.0;
+    }
+    if engine
+        .keyboard_state
+        .pressed_any(&[KeyCode::Down, KeyCode::S, KeyCode::O])
+    {
+        direction -= 1.0;
+    }
+
+    // Move player objects
+    let player = engine.sprites.get_mut("player").unwrap();
+    player.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player.rotation = direction * 0.15;
+    if player.translation.y < -360.0 || player.translation.y > 360.0 {
+        game_state.health_amount = 0;
+    }
+
+    // Move road objects
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -675.0 {
+                sprite.translation.x += 1500.0;
+            }
+        }
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
+        }
+    }
+
+    // Deal with collisions
+    let health_message = engine.texts.get_mut("health_message").unwrap();
+    for event in engine.collision_events.drain(..) {
+        if !event.pair.either_contains("player") || event.state.is_end() {
+            continue;
+        }
+        if game_state.health_amount > 0 {
+            game_state.health_amount -= 1;
+            health_message.value = format!("Health: {}", game_state.health_amount);
+            engine.audio_manager.play_sfx(SfxPreset::Impact3, 0.5);
+        }
+    }
+    if game_state.health_amount == 0 {
+        game_state.lost = true;
+        let game_over = engine.add_text("game over", "Game Over");
+        game_over.font_size = 128.0;
+        engine.audio_manager.stop_music();
+        engine.audio_manager.play_sfx(SfxPreset::Jingle3, 0.5);
+    }
+}
